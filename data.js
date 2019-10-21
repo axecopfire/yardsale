303fdc594769d2bf3030534ab31da60fb1f464ee
axecopfire <sbraun990@gmail.com>
2019-10-21 14:11:05 -0400

    init

---
+++ data.js
@@ -0,0 +1,129 @@
+var data = [
+    {
+        "name": "patio chairs",
+        "desc": "Outdoor Patio chairs. They recline and have cup holders.",
+        "count": 2,
+        "cat": ["furniture"]
+    },
+    {
+        "name": "water hose, nozzle, and reel",
+        "desc": "~50ft water hose, variable setting nozzle, and place to store it",
+        "cat": ["outdoor", "garden"]
+    },
+    {
+        "name": "irrigation hose, timer",
+        "desc": "~20ft slow drip hose with a timer",
+        "cat": ["garden"]
+    },
+    {
+        "name": "electric string lawn mower",
+        "desc": "electric lawn mower. Have extra string and reel.",
+        "cat": ["outdoor"]
+    },
+    {
+        "name": "150ft extension cord and reel",
+    },
+    {
+        "name": "75ft extension cord and reel",
+    },
+    {
+        "name": "fishing supplies",
+        "desc": "tackle boxes with tackle, poles, and reels",
+        "cat": ["outdoor"]
+    },
+    {
+        "name": "3 tiered side table",
+        "desc": "wooden 3 tiered side table",
+        "cat": ["furniture"]
+    },
+    {
+        "name": "small rolling wooden cabinet",
+        "cat": ["furniture"]
+    },
+    {
+        "name": "record player",
+        "desc": "ION Profile Pro. Missing 1 foot. Works great.",
+        "cat": ["electronics"]
+    },
+    {
+        "name": "laptop",
+        "desc": "Samsung laptop needs hard drive replaced. Battery doesn't hold much of a charge either",
+        "cat": ["electronics"]
+    },
+    {
+        "name": "board/card games",
+        "desc": "Pathfinder, Cthulu Flux, Dominion Intrigue, Elder Signs",
+        "cat": ["games"]
+    },
+    {
+        "name": "metal storage racks",
+        "desc": "racks used for garage and outside storage",
+    },
+    {
+        "name": "white table",
+        "desc": "Table with a flip up mirror and two drawers",
+        "cat": ["furniture"]
+    },
+    {
+        "name": "locally made chest",
+        "desc": "Bought it at Art on Broad. Reallly nice locally made chest",
+        "cat": ["furniture"]
+    },
+    {
+        "name": "Kelty backpack",
+        "desc": "External frame pack."
+    },
+    {
+        "name": "crock pot",
+        "desc": "basic crock pot"
+    },
+    {
+        "name": "Love Sac",
+        "desc": "Love Sac is a big ol bean bag. They have white fluffy slip covers",
+        "count": 2,
+        "cat": ["furniture"]
+    },
+    {
+        "name": "motorcycle helmets",
+        "desc": "1 small, 1 medium, 1 large",
+        "cat": ["automotive"],
+        "count": 3
+    },
+    {
+        "name": "work desk",
+        "desc": "2 tiered metal legged work desk",
+        "cat": ["furniture"]
+    },
+    {
+        "name": "growing bags",
+        "desc": "They're like garden pots but made of bio degradable bags. (They haven't degraded yet).",
+        "count": 5,
+        "cat": ["garden"]
+    },
+    {
+        "name": "party cooler",
+        "desc": "rolling party cooler. Can hold a ton of party. Perfect for a week at Bonarroo",
+        "cat": ["outside"]
+    },
+    {
+        "name": "canopy",
+        "desc": "One of those pop up outdoors shade things. Perfect for outdoor parties at Bonarroo",
+        "cat": ["outside"]
+    },
+    {
+        "name": "yard supplies",
+        "desc": "shovel, rake, axe/hoe combo, loppers, sheers",
+        "cat": ["outside", "garden"]
+    }, 
+    {
+        "name": "fishing stuff",
+        "desc": "tackle box, tackle, fishing poles, reels",
+        "count": 2,
+        "cat": ["outdoor"]
+    },
+    {
+        "name": "car care stuff",
+        "desc": "cleaners, fluids, sprays, wipes, towels",
+        "cat": ["automotive"]
+    }
+]
